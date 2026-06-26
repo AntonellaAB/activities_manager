d83d7627fab052dbd6893295bde16faed4adc80b
Vicente Gomez de la Fuente <vicenteenrique.365@gmail.com>
2026-06-25 22:48:36 -0300

    Add files via upload

---
+++ models/horas_extension.py
@@ -0,0 +1,14 @@
+
+class HorasExtension:
+    def __init__(self, id=None, alumno_id=None, semestre=None, actividad_id =None, ubicacion=None, materia_id=None, profesor_id=None, fecha=None, horas=None, informe=None, fecha_registro=None):
+        self.id = id
+        self.alumno_id = alumno_id
+        self.semestre = semestre
+        self.actividad_id = actividad_id
+        self.ubicacion = ubicacion
+        self.materia_id = materia_id
+        self.profesor_id = profesor_id
+        self.fecha = fecha
+        self.horas = horas
+        self.informe = informe
+        self.fecha_registro = fecha_registro
